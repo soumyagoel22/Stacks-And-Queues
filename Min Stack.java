aa55445550ba59f01004537b555411c78fc65987
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-08-14 18:26:07 +0530

    Create Min Stack.java

---
+++ Min Stack.java
@@ -0,0 +1,32 @@
+class Solution {
+    Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minstack = new Stack<Integer>();
+    public void push(int x) {
+        stack.push(x);
+        if(minstack.empty() || x<=minstack.peek())
+        minstack.push(x);
+    }
+
+    public void pop() {
+        if(!stack.empty())
+        {
+            int num = stack.pop();
+            if(num==minstack.peek())
+            {
+                minstack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        if(stack.empty())
+        return -1;
+        return stack.peek();
+    }
+
+    public int getMin() {
+        if(minstack.empty())
+        return -1;
+        return minstack.peek();
+    }
+}
